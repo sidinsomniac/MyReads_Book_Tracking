4c123aef60cd5d2d4cc71e49714224cb5f919ea4
sidinsomniac <chatterjeesid1993@gmail.com>
2018-11-18 17:37:15 +0530

    Break app into homepage and search page view components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
-import Libraries from './Libraries';
+import Searchpage from './views/SearchPage';
+import Homepage from './views/Homepage';
 
 class BooksApp extends React.Component {
   state = {
@@ -17,42 +18,7 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <Libraries/>
-              </div>
-            </div>
-            <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
-            </div>
-          </div>
-        )}
+        {this.state.showSearchPage ? <Searchpage/> : <Homepage/>}
       </div>
     )
   }
--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,9 +1,22 @@
 import React,{Component} from 'react'
+import Libraries from '../Libraries';
 
 class Homepage extends Component {
 	render() {
 		return (
-			<div>Homepage</div>
+			<div className="list-books">
+				<div className="list-books-title">
+				<h1>MyReads</h1>
+				</div>
+				<div className="list-books-content">
+				<div>
+					<Libraries/>
+				</div>
+				</div>
+				<div className="open-search">
+				<button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+				</div>
+			</div>
 		)
 	}
 }
