6cb4da67620fe58f7db0021a7cca5813a04bdbb9
sidinsomniac <chatterjeesid1993@gmail.com>
2018-11-18 17:04:25 +0530

    Initialize smaller components for App

---
+++ src/Libraries.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+
+class Libraries extends Component {
+	render() {
+		return (
+			<div>Library</div>
+		)
+	}
+}
+
+export default Libraries
---
+++ src/views/SearchPage.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+
+class Searchpage extends Component {
+	render() {
+		return (
+			<div>Searchpage</div>
+		)
+	}
+}
+
+export default Searchpage
