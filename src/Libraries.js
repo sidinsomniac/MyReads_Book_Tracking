f5f05c21dc7ba94d55eff023d0ebc980c1cdbbb2
sidinsomniac <chatterjeesid1993@gmail.com>
2018-11-18 17:31:05 +0530

    Create book and library component for app

--- src/Libraries.js
+++ src/Libraries.js
@@ -1,9 +1,17 @@
 import React,{Component} from 'react'
+import Books from './Books';
 
 class Libraries extends Component {
 	render() {
 		return (
-			<div>Library</div>
+			<div className="bookshelf">
+				<h2 className="bookshelf-title">Currently Reading</h2>
+				<div className="bookshelf-books">
+				<ol className="books-grid">
+					<Books/>
+				</ol>
+				</div>
+			</div>
 		)
 	}
 }
