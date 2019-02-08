0edb860fa6cc366a92ba3220f074f89c500004a9
sidinsomniac <chatterjeesid1993@gmail.com>
2019-02-09 01:10:18 +0530

    Updates

---
+++ precache-manifest.2c8aa9cb820579c08435d45e6c33011d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b33ba0136b06239e38b2",
+    "url": "/static/js/main.b33ba013.chunk.js"
+  },
+  {
+    "revision": "2cce8bce1daf80bd16b5",
+    "url": "/static/js/1.2cce8bce.chunk.js"
+  },
+  {
+    "revision": "b33ba0136b06239e38b2",
+    "url": "/static/css/main.84ba0c10.chunk.css"
+  },
+  {
+    "revision": "6587adfa7ab75ef98d7e033fbf6becd0",
+    "url": "/index.html"
+  }
+];
