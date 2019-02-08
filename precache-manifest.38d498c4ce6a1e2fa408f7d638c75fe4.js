ce125e2ac720d6bb1c3001118e9069e907514f53
sidinsomniac <chatterjeesid1993@gmail.com>
2019-02-09 01:40:17 +0530

    Updates

---
+++ precache-manifest.38d498c4ce6a1e2fa408f7d638c75fe4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/MyReads_Book_Tracking/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/MyReads_Book_Tracking/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/MyReads_Book_Tracking/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "21e0d7f717c7daeadc2f",
+    "url": "/MyReads_Book_Tracking/static/js/runtime~main.21e0d7f7.js"
+  },
+  {
+    "revision": "0f9804cde3f133b3d7e5",
+    "url": "/MyReads_Book_Tracking/static/js/main.0f9804cd.chunk.js"
+  },
+  {
+    "revision": "2cce8bce1daf80bd16b5",
+    "url": "/MyReads_Book_Tracking/static/js/1.2cce8bce.chunk.js"
+  },
+  {
+    "revision": "0f9804cde3f133b3d7e5",
+    "url": "/MyReads_Book_Tracking/static/css/main.a062bf24.chunk.css"
+  },
+  {
+    "revision": "732442200b41e182302f29df19e7593e",
+    "url": "/MyReads_Book_Tracking/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2c8aa9cb820579c08435d45e6c33011d.js"
+  "/MyReads_Book_Tracking/precache-manifest.38d498c4ce6a1e2fa408f7d638c75fe4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/MyReads_Book_Tracking/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
